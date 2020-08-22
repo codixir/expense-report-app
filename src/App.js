16ad519de5e9a37f8ddd6110f05538ea6f0eb566
codixir <codixir@gmail.com>
2020-08-22 06:22:02 -0400

    Set expense data structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ExpenseForm from './components/ExpenseForm/ExpenseForm';
 import './App.css';
 
+const expenses = {
+  {
+    data: [
+    ],
+    maxNumOfExpenses: 5,
+    maxTotalExpense: {
+      amount: 1000,
+      currency: 'CAD',
+    }
+    totalExpense: 0
+  }
+};
+
 function App() {
+
   return (
     <div className="App">
       <ExpenseForm />
