74614885ca54583537a2b87d2454bf537a1083ec
codixir <codixir@gmail.com>
2020-08-22 08:02:01 -0400

    Implement submit expenses, fix currentTotal logic, add validation, and style button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import ExpenseForm from './components/ExpenseForm/ExpenseForm';
 import ExpenseList from './components/ExpenseList/ExpenseList';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 const expenses = {
-    data: [
-    ],
-    maxNumOfExpenses: 5,
-    maxTotalExpense: {
-      amount: 1000,
-      currency: 'CAD',
-    },
-    totalExpense: 0
+  data: [
+  ],
+  maxNumOfExpenses: 5,
+  maxTotalExpense: {
+    amount: 1000,
+    currency: 'CAD',
+  },
+  totalExpense: 0
 };
 
 function App() {
   const [expenseData, setData] = useState([]);
   const add = (data) => {
     expenses.data.push(data);
-
     setData([...expenses.data]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(JSON.stringify(expenses));
   };
   
   useEffect(() => {
@@ -28,8 +35,11 @@ function App() {
 
   return (
     <div className="App">
+      <Form class="form" onSubmit={handleSubmit}>
+        <Button type="submit" className="btn-submit-expenses">Submit Expenses</Button>  
+      </Form>
       <ExpenseForm expenses={expenses} add={add} />
-      <ExpenseList expenseData={expenseData}/>
+      <ExpenseList expenseData={expenseData}/>        
     </div>
   );
 }
--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import './ExpenseForm.css';
 
 const ExpenseForm = ({ expenses, add }) => {
   const [desc, setDesc] = useState('');
-  const [amount, setAmount] = useState('');
+  const [amount, setAmount] = useState(0);
   const [currency, setCurrency] = useState('');
   const [currentTotal, setCurrentTotal] = useState(0);
+  const [isValid, setValidation] = useState(true);
+  const [isDisabled, setDisabled] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if ((!desc.length || desc == ' ') || amount < 0 || (!currency.length || currency == ' ')) {
+      setValidation(false);
+      return;
+    }
+
+    setValidation(true);
 
     const expense = {
       description: desc,
@@ -21,15 +32,23 @@ const ExpenseForm = ({ expenses, add }) => {
 
     const data = expenses.data;
 
-    if (data.length) {
-      let total = data.reduce( ( sum, { amount } ) => sum + amount , 0)
-      setCurrentTotal(total);
+    if (data.length < 5 && currentTotal <= 1000 && currentTotal == 0) {
+      if (currentTotal == 0) {
+        setCurrentTotal(expense.amount);
+      }
+  
+      if (data.length) {
+        let total = data.reduce( ( sum, { amount } ) => sum + amount , 0)
+        setCurrentTotal(total + expense.amount);
+      }
     }
     
-    if (data.length < 5 && expense.amount <= 1000 && currentTotal <= 1000) {
+    if (data.length < 5 && currentTotal <= 1000) {
       expense.id = data.length;
       add(expense);
-    } 
+    } else {
+      setDisabled(true);
+    }
         
     setDesc('');
     setAmount(0);
@@ -37,37 +56,44 @@ const ExpenseForm = ({ expenses, add }) => {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group as={Row} controlId="description">
-        <Form.Label column sm={2}>
-          Description
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="text" value={desc} placeholder="Description" onChange={e => setDesc(e.target.value)}/>
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} controlId="amount">
-        <Form.Label column sm={2}>
-          Amount
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="text" value={amount} placeholder="Amount" onChange={e => setAmount(Number(e.target.value))} />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} controlId="currency">
-        <Form.Label column sm={2}>
-          Currency
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="text" value={currency} placeholder="Currency" onChange={e => setCurrency(e.target.value)}/>
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row}>
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button type="submit">Submit Expense</Button>
-        </Col>
-      </Form.Group>
-    </Form>
+    <>
+      {
+        isValid ? '': <Alert variant="danger">Enter valid values</Alert>
+      }
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group as={Row} controlId="description">
+          <Form.Label column sm={2}>
+            Description
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" value={desc} placeholder="Description" onChange={e => setDesc(e.target.value)}/>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="amount">
+          <Form.Label column sm={2}>
+            Amount
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="number" value={amount} placeholder="Amount" onChange={e => setAmount(Number(e.target.value))} />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} controlId="currency">
+          <Form.Label column sm={2}>
+            Currency
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" value={currency} placeholder="Currency" onChange={e => setCurrency(e.target.value)}/>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button disabled={isDisabled} type="submit">Add</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+      <p className="current-total">Total: {currentTotal}</p>
+    </>
   )
 };
 
