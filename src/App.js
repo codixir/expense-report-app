a4ca4deadde279c1413498a178d88516cc3c975a
codixir <codixir@gmail.com>
2020-08-22 06:24:08 -0400

    Fix syntax error

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import ExpenseForm from './components/ExpenseForm/ExpenseForm';
 import './App.css';
 
 const expenses = {
-  {
     data: [
     ],
     maxNumOfExpenses: 5,
     maxTotalExpense: {
       amount: 1000,
       currency: 'CAD',
-    }
+    },
     totalExpense: 0
-  }
 };
 
 function App() {
